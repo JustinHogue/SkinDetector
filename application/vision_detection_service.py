15a929cf6946013279469198bc96d5a2b49ff374
JustinHogue <justin.hogue.2@ulaval.ca>
2022-04-24 15:53:16 -0400

    Initial commit and structure of the code

---
+++ application/vision_detection_service.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+class VisionDetectionService:
+    def __init__(self) -> None:
+        """
+        Constructor for class VisionDetectionService
+        """
+        pass
+
+    def analyze_frame_for_skin(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Analyzes a frame in order to detect the human skin
+        :param frame: The frame to analyze
+        :return: The new frame with detected skin only
+        """
+        # Adding a border to detect the skin on the edge
+        borderType = cv2.BORDER_CONSTANT
+        top = int(0.006 * frame.shape[0])  # shape[0] = rows
+        bottom = top
+        left = int(0.002 * frame.shape[1])  # shape[1] = cols
+        right = left
+        border_color = (0, 0, 0)
+        img = cv2.copyMakeBorder(frame, top, bottom, left, right, borderType, None, border_color)
+
+        # We transfer into HSV
+        frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        
+        # We filter the image after smoothing it
+        blurred_frame = cv2.medianBlur(frame_hsv, 7)
+        skin = cv2.inRange(blurred_frame, (110, 10, 50), (170, 70, 255))
+        
+        # We treat the holes in the filtered frame
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv2.morphologyEx(skin, cv2.MORPH_OPEN, kernel)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+
+        return closing
---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+from application.vision_detection_service import VisionDetectionService
+
+
+def main():
+    video_capture = cv2.VideoCapture(0)
+    while True:
+        # Capture frame-by-frame
+        _, frame = video_capture.read()
+
+        # Detect the skin with the vision service
+        vision_service = VisionDetectionService()
+        detected_skin = vision_service.analyze_frame_for_skin(frame)
+
+        # Display the resulting frame
+        cv2.imshow('frame', detected_skin)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # When everything done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
